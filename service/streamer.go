15517982caa1c9131be767d3d761509a29b5eed4
Nicolas Perraut <n.perraut@gmail.com>
2019-02-28 12:46:45 +0100

    ♻️ (streamer)Split Serve function

--- service/streamer.go
+++ service/streamer.go
@@ -41,60 +41,73 @@ func (s *Streamer) Serve() {
 	s.log.Info(fmt.Sprintf("Streamer started. %d files found", len(files)))
 
 	for _, file := range files {
-		fileData, err := os.Open(fmt.Sprintf("%s/%s", s.sb.Config.Streamer.PlaylistDir, file.Name()))
-		//util.CheckError(err, s.log)
-
-		head := make([]byte, 261)
-		_, err = fileData.Read(head)
+		stream, format, err := s.getStream(file)
 
 		if err != nil {
-			s.log.Warn(fmt.Sprintf("File %s seems corrupted", file.Name()))
+			s.log.Warn(err)
 			continue
 		}
 
-		ft, err := filetype.Match(head)
+		s.streamToMessage(stream, format)
+	}
+}
 
-		if ft == filetype.Unknown {
-			continue
+func (s *Streamer) getStream(file os.FileInfo) (beep.StreamCloser, beep.Format, error) {
+	fileData, err := os.Open(fmt.Sprintf("%s/%s", s.sb.Config.Streamer.PlaylistDir, file.Name()))
+	util.CheckError(err, s.log)
+
+	head := make([]byte, 261)
+	_, err = fileData.Read(head)
+
+	if err != nil {
+		s.log.Warn(fmt.Sprintf("File %s seems corrupted", file.Name()))
+		return nil, beep.Format{}, err
+	}
+
+	ft, err := filetype.Match(head)
+
+	if ft == filetype.Unknown {
+		return nil, beep.Format{}, fmt.Errorf("unknown file type")
+	}
+
+	var stream beep.StreamCloser
+	var format beep.Format
+
+	s.log.Debug(fmt.Sprintf("File %s has type %s", file.Name(), ft.MIME.Value))
+
+	if ft.MIME.Value == "audio/x-xav" {
+		stream, format, err = wav.Decode(fileData)
+		util.CheckError(err, s.log)
+	} else if ft.MIME.Value == "audio/mpeg" {
+		stream, format, err = mp3.Decode(fileData)
+		util.CheckError(err, s.log)
+	}
+	s.log.Info(fmt.Sprintf("Audio format is: channels=%d, sampleRate=%d, precision=%d", format.NumChannels, format.SampleRate, format.Precision))
+
+	return stream, format, nil
+}
+
+func (s *Streamer) streamToMessage(stream beep.StreamCloser, format beep.Format) {
+	buff := make([][2]float64, 512)
+
+	ok := true
+	n := 512
+
+	for ok == true {
+		n, ok = stream.Stream(buff)
+
+		samplesLeft := make([]float64, n)
+		samplesRight := make([]float64, n)
+
+		for i := 0; i < n; i++ {
+			samplesLeft[i] = buff[i][0]
+			samplesRight[i] = buff[i][1]
 		}
 
-		var stream beep.StreamCloser
-		var format beep.Format
+		msg := &message.StreamData{SamplesLeft: samplesLeft, SamplesRight: samplesRight}
+		msgData, _ := message.ToBuffer(msg)
+		s.Messenger.Message <- &message.WriteRequest{DeviceName: "*", Message: msgData}
 
-		s.log.Debug(fmt.Sprintf("File %s has type %s", file.Name(), ft.MIME.Value))
-
-		if ft.MIME.Value == "audio/x-xav" {
-			stream, format, err = wav.Decode(fileData)
-			util.CheckError(err, s.log)
-		} else if ft.MIME.Value == "audio/mpeg" {
-			stream, format, err = mp3.Decode(fileData)
-			util.CheckError(err, s.log)
-		}
-		s.log.Info(fmt.Sprintf("Audio format is: channels=%d, sampleRate=%d, precision=%d", format.NumChannels, format.SampleRate, format.Precision))
-
-		buff := make([][2]float64, 512)
-
-		ok := true
-		n := 512
-
-		for ok == true {
-			n, ok = stream.Stream(buff)
-
-			samplesLeft := make([]float64, n)
-			samplesRight := make([]float64, n)
-
-			for i := 0; i < n; i++ {
-				samplesLeft[i] = buff[i][0]
-				samplesRight[i] = buff[i][1]
-			}
-
-			msg := &message.StreamData{SamplesLeft: samplesLeft, SamplesRight: samplesRight}
-			msgData, _ := message.ToBuffer(msg)
-			s.Messenger.Message <- &message.WriteRequest{DeviceName: "*", Message: msgData}
-
-			time.Sleep(format.SampleRate.D(n))
-		}
-
-		_ = fileData.Close()
+		time.Sleep(format.SampleRate.D(n))
 	}
 }
